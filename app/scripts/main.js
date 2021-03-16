fcf5d0ef3f05d6b7886e43ec667ba589578d9ccf
Eugene <eugene.kotsarev@gmail.com>
2021-03-16 12:07:49 +0300

    upd icons, sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,5 @@
 import './src/core/widget';
 import './src/core/observer';
-// import './src/core/uikit/uikit-drawer';
-// import './src/core/uikit/uikit-scroll-header';
-// import './src/core/uikit/uikit-autogenerator';
-// import './src/core/uikit/uikit-copy-to-clipboard';
-// import './src/core/uikit/uikit-scroll-to';
-// import './src/core/uikit/uikit-accord';
 
 import './src/utils/helpers';
 import './src/utils/device';
@@ -17,22 +11,12 @@ import './src/utils/scroll-to';
 import './src/utils/layout';
 import './src/utils/preloader';
 import './src/utils/vh-hack';
-// import './src/utils/mobile-debug';
-// import './src/utils/input-filter';
-
-// import './src/widgets/example';
 import './src/widgets/accord';
 
 import './src/widgets/popups';
 import './src/widgets/video-link';
-// import './src/widgets/gallery';
 import './src/widgets/gallery-slider';
 import './src/widgets/base-slider';
-
-// import './src/widgets/inputs/numeric-input';
-// import './src/widgets/inputs/masked-input';
-// import './src/widgets/inputs/datepicker';
-// import './src/widgets/inputs/select';
 
 import './src/app';
 
